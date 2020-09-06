320f0ca5c87bd24281ba7358657405d39b1a3f5c
sanvean74 <dlambertpdx@gmail.com>
2020-09-06 15:20:13 -0700

    server setup

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 require('dotenv').config();
-require('./lib/utils/connect')();
+// require('./lib/utils/connect')();
+
 const app = require('./lib/app');
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 7890;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
